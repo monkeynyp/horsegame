f1741d58cf79d9c792650b3b65bbf7bfe7b5a57a
Louis Ngai <thinking.monkey@gmail.com>
2023-12-28 20:20:48 +0800

    create the absolute path

--- racecard/views.py
+++ racecard/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 import pandas as pd
+import os
+from django.conf import settings
 
 # Create your views here.
 def races(request):
-     current_race = pd.read_csv("horsegame/data/current_race.csv")
+     csv_path = os.path.join(settings.BASE_DIR, "racecard/data/current_race.csv")
+     current_race = pd.read_csv(csv_path)
      template = loader.get_template("currentrace.html")
      return HttpResponse(template.render({"current_race":current_race},request))
