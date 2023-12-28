e06dbae5830bf26b3e621674dd3fc77ae1041140
Louis Ngai <thinking.monkey@gmail.com>
2023-12-28 12:00:19 +0800

    Update Allowed Host

--- horsegame/settings.py
+++ horsegame/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = "django-insecure-9h)=u=$e*fqnu3#l+3_i9h8iz$125pm01h)r8s@um3r#yqx(&7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['.pythonanywhere.com']
 
 # Application definition
 
