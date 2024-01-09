e2dbf6bcb505afa81c6d1dac71e0d3f2e9e0141a
Louis Ngai <thinking.monkey@gmail.com>
2024-01-10 00:00:20 +0800

    Update URL file

--- racecard/urls.py
+++ racecard/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('', views.about, name='about'),
     path('racecard/', views.racecard, name='racecard'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
+    
     ]
 
