e59d5130d2e9ab024714774b4022e020311318f6
Louis Ngai <thinking.monkey@gmail.com>
2024-03-12 10:12:07 +0800

    Update User Hit Weight Calculation

--- racecard/management/commands/update_user_weight.py
+++ racecard/management/commands/update_user_weight.py
@@ -1,56 +1,27 @@
 from django.core.management.base import BaseCommand
 from racecard.models import UserTips,UserScores  # Replace 'YourModel' with the actual model name
-from datetime import datetime
+
 import pandas as pd
-import os
-from django.conf import settings
+
+
 from django.db.models import Sum, Count, F, ExpressionWrapper, FloatField, Window, Max, Subquery, OuterRef
-from django.db.models.functions import Lag, RowNumber
-from datetime import timedelta
+
 
 
 class Command(BaseCommand):
     help = 'Update data from CSV files to SQLite database'
 
- 
 
     def add_arguments(self, parser):
         # Add command line arguments
    #     parser.add_argument('num_races', type=int, help='Number of races')
+        
+        
         parser.add_argument('race_date', type=str, help='Race date in YYYY/MM/DD format')
     
     def handle(self, *args, **options):
 
         race_date = options['race_date']
-        csv_path = os.path.join(settings.BASE_DIR, "racecard/data/race_hist_update.csv")
-        csv_data = pd.read_csv(csv_path)
-        print(csv_data[['HorseName','Place']])
-        # Filter the CSV data for places 1, 2, or 3
-        csv_data['Place'] = pd.to_numeric(csv_data['Place'], errors='coerce')
-        filtered_data = csv_data[(csv_data['Place'] == 1) | (csv_data['Place'] == 2) | (csv_data['Place'] == 3)]
-        print("filter123")
-        print(filtered_data[["RaceDate","RaceNo","HorseName","RaceNo","Place"]])
-        filtered_data = filtered_data[(filtered_data["RaceDate"]==race_date)]
-        print("filterDate")
-        print(filtered_data[["RaceDate","RaceNo","HorseName","RaceNo","Place"]])
-        # Iterate through the filtered data and update UserTips records
-        for index, row in filtered_data.iterrows():
-            race_date = datetime.strptime(row['RaceDate'], '%Y/%m/%d').date()
-            race_no = row['RaceNo']
-            horse_name = row['HorseName']
-            dividend = row['Dividend']
-            
-            # Update UserTips records
-            UserTips.objects.filter(
-                horse_name=horse_name,
-                race_date=race_date,
-                race_no=race_no,  
-            ).update(hit=1,dividend=dividend)
-
-
-        # Get the UserTips data grouped by user, with the total records and total hits
-# Calculate the hit_weight for each user
-
  
 
 # Subquery to get the maximum total records
