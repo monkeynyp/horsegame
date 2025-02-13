1e27ff82387f19826cea9caf02853faa5e2b64cb
Louis Ngai <thinking.monkey@gmail.com>
2025-02-13 16:22:50 +0800

    updated sitemap

--- racecard/sitemaps.py
+++ racecard/sitemaps.py
@@ -1,12 +1,12 @@
 from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
-from .models import Article, Race
+from .models import Article, Race, StockInfo
 from datetime import datetime
 
 # Dictionary to store last modification dates for static pages
 lastmod_dates = {
-    'recent_article': datetime(2024, 10, 10),
-    'racecard': datetime(2025, 1, 28),  # Update this date to the correct last modification date
+    'recent_article': datetime(2025, 2, 10),
+    'racecard': datetime(2025, 2, 19),  # Update this date to the correct last modification date
     'racecard_vip': datetime(2025, 1, 18),  # Example update
     'jockey_king': datetime(2025, 1, 19),  # Example update
     # Add other static pages and their last modification dates here
@@ -21,7 +21,7 @@ class StaticViewSitemap(Sitemap):
             'recent_article', 'racecard', 'racecard_vip', 'jockey_king', 'trainer_king',
             'view_by_member', 'newsletter', 'contact', 'help', 'privacy', 'disclaimer',
             'responsible', 'match_chart', 'data_update_console', 'lottory_predict',
-            'ichi_lotto', 'lotto_next_stat', 'lotto_test', 'lotto_trio', 'lotto_longterm'
+            'ichi_lotto', 'lotto_next_stat', 'lotto_test', 'lotto_trio', 'lotto_longterm','stock_info'
         ]
 
     def location(self, item):
@@ -46,6 +46,16 @@ class RaceSitemap(Sitemap):
 
     def items(self):
         return Race.objects.all()
+
+    def lastmod(self, obj):
+        return obj.date or datetime(2000, 1, 1)  # Default date if date is None
+
+class StockInfoSitemap(Sitemap):
+    changefreq = 'weekly'
+    priority = 0.6
+
+    def items(self):
+        return StockInfo.objects.all()
 
     def lastmod(self, obj):
         return obj.date or datetime(2000, 1, 1)  # Default date if date is None
